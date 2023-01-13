94be54d87764c4deead79d08370a83dd029ea367
Tripathi, Dhruv Prakash <dhruv.tripathi@intel.com>
2023-01-13 07:45:18 -0800

    Run arp-proxy from within the script for automation
    Signed-off-by: Tripathi, Dhruv Prakash <dhruv.tripathi@intel.com>

--- scripts/arp_proxy.sh
+++ scripts/arp_proxy.sh
@@ -5,4 +5,6 @@ ip netns add pod0
 ip link set P4TAP_0 netns pod0
 ip netns exec pod0 ifconfig P4TAP_0 169.254.1.1/16 up
 ip netns exec pod0 ip route add default dev P4TAP_0 scope link
-ip netns exec pod0 /bin/bash
+
+export ARP_PROXY_IF=P4TAP_0
+ip netns exec pod0 ./bin/arp_proxy &> /dev/null &
